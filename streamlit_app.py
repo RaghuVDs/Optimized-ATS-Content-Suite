8912d9ca5125bcae8cfc4a0bb910f7aee68c6b83
Raghuveera Narasimha <147996831+RaghuVDs@users.noreply.github.com>
2025-03-27 01:31:51 +0000

    main changes made

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,4 @@
 import streamlit as st
 from app import main 
 
-
-with st.sidebar:
-    selected_page = st.radio("Select a page", ["app"])
-# Display the selected page
-if selected_page == "app":
-    main()
+main()
